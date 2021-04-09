cd5bf2bfef9a64eb32cb3b4a94e8fe09e81b85ed
P056_amai <P056_amai@LAPTOP-7O5H7RT7>
2021-04-09 23:54:28 +0900

    2021/04/09の復習（商品マスタ）

---
+++ 商品マスタ/WebContent/jsp/itemsMaster.jsp
@@ -0,0 +1,39 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>商品マスタ一覧</title>
+</head>
+<body>
+	<h1>商品マスタ 設計</h1>
+
+	<input type="submit" valus="新規作成" />
+
+	<table border="1">
+		<tr>
+			<th>Ｎｏ．</th>
+			<th>商品ＩＤ</th>
+			<th>商品名</th>
+			<th>商品単価</th>
+			<th>削除</th>
+		</tr>
+		<tr>
+			<%
+			for (ItemsBean bn : list) {
+			%>
+				<td><%=bn.getItem_no()%></td>
+				<td><%=bn.getItem_id()%></td>
+				<td><%=bn.getItem_name()%></td>
+				<td><%=bn.getItem_price()%></td>
+				<td><input type="submit" valus="削除" /></tr>
+			<%
+			}
+			%>
+
+	</table>
+
+
+</body>
+</html>
---
+++ 商品マスタ/src/商品マスタ/ItemsBean.java
@@ -0,0 +1,66 @@
+package 商品マスタ;
+
+public class ItemsBean {
+
+	private int item_no;
+	private String item_id;
+	private String item_name;
+	private int item_price;
+
+	/**
+	 * @return item_no
+	 */
+	public int getItem_no() {
+		return item_no;
+	}
+
+	/**
+	 * @param item_no セットする item_no
+	 */
+	public void setItem_no(int item_no) {
+		this.item_no = item_no;
+	}
+
+	/**
+	 * @return item_id
+	 */
+	public String getItem_id() {
+		return item_id;
+	}
+
+	/**
+	 * @param item_id セットする item_id
+	 */
+	public void setItem_id(String item_id) {
+		this.item_id = item_id;
+	}
+
+	/**
+	 * @return item_name
+	 */
+	public String getItem_name() {
+		return item_name;
+	}
+
+	/**
+	 * @param item_name セットする item_name
+	 */
+	public void setItem_name(String item_name) {
+		this.item_name = item_name;
+	}
+
+	/**
+	 * @return item_price
+	 */
+	public int getItem_price() {
+		return item_price;
+	}
+
+	/**
+	 * @param item_price セットする item_price
+	 */
+	public void setItem_price(int item_price) {
+		this.item_price = item_price;
+	}
+
+}
